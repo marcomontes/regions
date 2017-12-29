792006aa4b04ab7e7a0a6f1cc7cc6db652382fc6
Marco Montes <marcomontes@gmail.com>
2017-12-29 12:04:02 -0500

    update regions method

--- app/models/region.rb
+++ app/models/region.rb
@@ -6,4 +6,7 @@ class Region < ApplicationRecord
   validates :numeric_code, presence: true
   validates :numeric_code, uniqueness: true
 
+  def update_municipalities(new_municipalities)
+    new_municipalities.each{|m| municipalities << m unless municipalities.include?(m) }
+  end
 end
