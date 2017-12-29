7e6a8b3df0df7f63ecb87eb8c8c936ba6dbb0e0a
Marco Montes <marcomontes@gmail.com>
2017-12-29 12:03:22 -0500

    add helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def municipalities_names(region)
+    region.municipalities.collect(&:name).join(',')
+  end
+
+  def regions_names(municipality)
+    municipality.regions.collect(&:name).join(',')
+  end
+
 end
