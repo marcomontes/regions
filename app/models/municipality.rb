96c0702e62312355da116d54b124c8433c9fd051
Marco Montes <marcomontes@gmail.com>
2017-12-29 11:26:03 -0500

    Add model validations

--- app/models/municipality.rb
+++ app/models/municipality.rb
@@ -2,6 +2,10 @@ class Municipality < ApplicationRecord
 
   has_and_belongs_to_many :regions
 
+  validates :name, presence: true
+  validates :numeric_code, presence: true
+  validates :numeric_code, uniqueness: true
+
   def get_state
     state ? 'Activo' : 'Inactivo'
   end
--- app/models/region.rb
+++ app/models/region.rb
@@ -2,4 +2,8 @@ class Region < ApplicationRecord
 
   has_and_belongs_to_many :municipalities
 
+  validates :name, presence: true
+  validates :numeric_code, presence: true
+  validates :numeric_code, uniqueness: true
+
 end
