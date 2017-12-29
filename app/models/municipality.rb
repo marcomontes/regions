24367e32b0598f9ced53df9b2fbdde0ab2b4b3bd
Marco Montes <marcomontes@gmail.com>
2017-12-29 09:50:08 -0500

    add models and relations

---
+++ app/models/municipality.rb
@@ -0,0 +1,5 @@
+class Municipality < ApplicationRecord
+
+  has_and_belongs_to_many :regions
+  
+end
---
+++ app/models/region.rb
@@ -0,0 +1,5 @@
+class Region < ApplicationRecord
+
+  has_and_belongs_to_many :municipalities
+
+end
